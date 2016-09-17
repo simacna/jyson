6dc9fe8cc4cbc0502fd482bfb906f7d4360f3523
sina sima <simacna@gmail.com>
2016-09-17 19:35:01 -0400

    playing around

--- dataVizPJS/sandpit/script.js
+++ dataVizPJS/sandpit/script.js
@@ -1,11 +1,19 @@
-// script.js
-var data = [3, 7, 2, 9, 1, 11];
-var total = 0;
-var sum = data.forEach(function(d){
-    alert(d);
-    total += d;
-});
-sum = total;
+// // script.js
+// var data = [3, 7, 2, 9, 1, 11];
+// var total = 0;
+// var sum = data.forEach(function(d){
+//     alert(d);
+//     total += d;
+// });
+// sum = total;
 
-console.log('Sum = ' + sum);
-// outputs 'Sum = 33'
+// console.log('Sum = ' + sum);
+// // outputs 'Sum = 33'
+
+//below is example of use strict
+//Q - passing an argument to iffy.. look into this
+(function(foo){
+  'use strict';
+
+
+}(window.foo = window.foo || {});
