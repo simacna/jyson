9d862d33594bd01a4776147b167a9a36fc5de869
sina sima <simacna@gmail.com>
2016-11-08 11:24:37 -0500

    testing code thats shit form the book

--- dataVizPJS/sandpit/3.py
+++ dataVizPJS/sandpit/3.py
@@ -1,5 +1,5 @@
 # f = open('data/nobel_winners.json', 'r')
-
+# nobel_winners = f[0]
 # cols = nobel_winners[0].keys()
 
 
@@ -10,10 +10,20 @@
 #         row = [str(o[col]) for col in cols]
 #         f.write(','.join(row) + '\n')
 
-import ast
-with open('data/nobel_winners.csv', 'r') as myfile:
-    data = myfile.read().replace('\n', '')
-data_dict = ast.literal_eval(data)
-# cols =  data_dict[0].keys()
+# import ast
+# with open('data/nobel_winners.csv', 'r') as myfile:
+#     data = myfile.read().replace('\n', '')
+# data_dict = ast.literal_eval(data)
+# # cols =  data_dict[0].keys()
 
-print(data_dict)
+# print(data_dict)
+
+import nw
+# from nw import *
+"""using import nw doesn't allow for accessing the nobel list vs
+using explicit import from nw import * gives access to the variable
+
+
+"""
+
+print(nobel)
